c6b56a49ecc6a106c9ab117cefad693a44b78e3c
Flamberge <thebears86@gmail.com>
2021-07-09 12:05:24 +0900

    2021 07 07

--- src/sample/controllers/replace/Content2Controller.java
+++ src/sample/controllers/replace/Content2Controller.java
@@ -3,10 +3,62 @@ package sample.controllers.replace;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import sample.controllers.DataController;
+import sample.model.Content2Model;
 
-public class Content2Controller {
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ResourceBundle;
+
+public class Content2Controller extends DataController {
     @FXML public Button changedBtn;
+    @FXML public TableColumn<Content2Model , Long> rowNum;
+    @FXML public TableColumn<Content2Model , String> isbn;
+    @FXML public TableColumn<Content2Model , String> bookName;
+    @FXML public TableColumn<Content2Model , String> publisher;
+    @FXML public TableColumn<Content2Model , String> data1;
+    @FXML public TableColumn<Content2Model , LocalDate> regDate;
+    @FXML public TableColumn<Content2Model , LocalTime> regTime;
+    @FXML public TableColumn c2;
+    @FXML public TableColumn c3;
+    @FXML public TableColumn c4;
+    @FXML public TableColumn c5;
+    @FXML public TableColumn c6;
+    @FXML public TableColumn c7;
+    @FXML public TableColumn c8;
+    @FXML public TableColumn c9;
+    @FXML public TableView<Content2Model> tab3Table;
+
+    @Override
+    public void initData(Object o) {
+
+        if( !(o instanceof Content2Model) ){
+            throw new IllegalArgumentException("실패 이새끼야.");
+        }
+
+        tab3Table.getItems().add((Content2Model) o);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        rowNum.setCellValueFactory      (cellData -> cellData.getValue().getRowNum());
+        isbn.setCellValueFactory        (cellData -> cellData.getValue().getIsbn());
+        bookName.setCellValueFactory    (cellData -> cellData.getValue().getBookName());
+        publisher.setCellValueFactory   (cellData -> cellData.getValue().getPublisher());
+        data1.setCellValueFactory       (cellData -> cellData.getValue().getData1());
+        regDate.setCellValueFactory     (cellData -> cellData.getValue().getRegDate());
+        regTime.setCellValueFactory     (cellData -> cellData.getValue().getRegTime());
+
+
+
+
+    }
 
     public void changedBtnAction(MouseEvent actionEvent){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
---
+++ src/sample/model/Content2Model.java
@@ -0,0 +1,34 @@
+package sample.model;
+
+import javafx.beans.property.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+public class Content2Model {
+
+    private ObjectProperty<Long> rowNum;
+    private StringProperty isbn;
+    private StringProperty bookName;
+    private StringProperty publisher;
+    private StringProperty data1;
+    private ObjectProperty<LocalDate> regDate;
+    private ObjectProperty<LocalTime> regTime;
+
+    @Builder
+    public Content2Model(long rowNum, String isbn, String bookName, String publisher, String data1) {
+        this.rowNum = new SimpleObjectProperty<Long>(rowNum);
+        this.isbn = new SimpleStringProperty(isbn);
+        this.bookName = new SimpleStringProperty(bookName);
+        this.publisher = new SimpleStringProperty(publisher);
+        this.data1 = new SimpleStringProperty(data1);
+        this.regDate = new SimpleObjectProperty<>(LocalDate.now());
+        this.regTime = new SimpleObjectProperty<>(LocalTime.now());
+    }
+}
