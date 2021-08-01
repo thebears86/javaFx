178991b83f2befadcf235d3f3bc13691f3503116
thebears86 <thebears86@gmail.com>
2021-08-02 03:06:47 +0900

    add layout example

---
+++ src/sample/defaultConfig/DefaultConfig.java
@@ -0,0 +1,8 @@
+package sample.defaultConfig;
+
+public class DefaultConfig {
+
+    public final static String defaultImagePath = "/icons/marketImg/zg3n4gcd.jpg";
+
+    public static final String CURRENCY = "$";
+}
